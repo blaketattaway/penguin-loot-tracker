a953149976787b62ea8eb0be72bbaa0e78d4f6f8
mapersico <marco.persico.94@gmail.com>
2025-05-02 19:21:28 -0300

    Fix: update config to use browser router with SPA app

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import App from "./features/app/App";
@@ -16,13 +16,13 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <HashRouter>
+      <BrowserRouter basename="/penguin-loot-tracker">
         <AuthProvider>
           <ThemeProvider>
             <App />
           </ThemeProvider>
         </AuthProvider>
-      </HashRouter>
+      </BrowserRouter>
     </QueryClientProvider>
   </StrictMode>
 );
