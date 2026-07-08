26fe100ab1b88ade34bec311e56686b2fdeef0ca
Diego Adolfo González Flores <dgonzalez@geovictoria.com>
2026-07-08 00:49:14 -0600

    Custom domain penguinloottracker.com (servir desde raiz)

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react(), viteSingleFile()],
   appType: "spa",
-  base: "/penguin-loot-tracker",
+  base: "/",
 });
